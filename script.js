dc3129bf41347f4792503d8d98db39f497fbfec9
lukeCruz <enio.cruz@yahoo.com.br>
2020-09-14 17:17:00 -0300

    refatoração de js html e css

--- script.js
+++ script.js
@@ -1,2 +1,37 @@
-//createobjectURL
-//
+
+//inserção de texto na imagem
+const textInput = document.querySelector('#text-input');
+const memeText = document.querySelector('#meme-text');
+textInput.addEventListener('input', function() {
+memeText.innerText = textInput.value;
+});
+//Inserção de imagem no container
+const memeInsert = document.querySelector('#meme-insert');
+const memeImage = document.querySelector('# meme-image');
+memeInsert.addEventListener('change', function(event) {
+
+    memeImage.src = URL.createObjectURL(event.target.files[0]);
+    memeImage.onload = function (){
+URL.revokeObjectURL(memeImage.src);
+    };
+});
+
+const pictureOne = document.querySelector('#meme-1');
+pictureOne.addEventListener('click', function(){
+memeImage.src = pictureOne.src;
+});
+
+const pictureTwo = document.querySelector('#meme-2');
+pictureOne.addEventListener('click', function(){
+memeImage.src = pictureOne.src;
+});
+
+const pictureTree = document.querySelector('#meme-3');
+pictureOne.addEventListener('click', function(){
+memeImage.src = pictureOne.src;
+});
+
+const pictureFour = document.querySelector('#meme-4');
+pictureOne.addEventListener('click', function(){
+memeImage.src = pictureOne.src;
+});
