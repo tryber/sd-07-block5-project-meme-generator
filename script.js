8939e3bff5dda9746e89442bd567c043790452c3
Juliana do Carmo <julianadocarmoti@gmail.com>
2020-09-14 09:35:27 -0300

    add event change. Change tags and styling css

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const textInput = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
+const fileSelector = document.getElementById('meme-insert');
+const imageMeme = document.getElementById('meme-image');
 
 textInput.addEventListener('input', function updateValue(event) {
   memeText.textContent = event.target.value;
+});
+//exibe a imagem no container
+fileSelector.addEventListener('change', function (event) {
+  const file = event.target.files[0];
+  imageMeme.src = URL.createObjectURL(file);
 });
