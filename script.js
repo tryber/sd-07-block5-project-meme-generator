69dc53e9ade686748fbaa8d5d4cff68d3c761201
Juliana do Carmo <julianadocarmoti@gmail.com>
2020-09-13 22:42:02 -0300

    add file script

---
+++ script.js
@@ -0,0 +1,6 @@
+let textInput = document.getElementById('text-input');
+let memeText = document.getElementById('meme-text');
+
+textInput.addEventListener('input', function updateValue (event) {
+    memeText.textContent = event.target.value
+});
