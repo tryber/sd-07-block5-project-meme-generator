4af117dff311dc0ba6853b9c2a957480f93f96dd
Adriano Forcellini <adrianoforcellinipedretti@gmail.com>
2020-09-14 14:54:52 -0300

    voltando ao commit 1, começar tudo denovo

--- script.js
+++
@@ -1,6 +0,0 @@
-let textInput = document.getElementById('text-input');
-let memeText = document.getElementById('meme-text');
-
-textInput.addEventListener('input', function updateValue(event) {
-    memeText.textContent = event.target.value
-});
