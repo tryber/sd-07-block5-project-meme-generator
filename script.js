af337530c4cc4d629f6d2ca57f9ae28fcc6f323a
Adriano Forcellini <adrianoforcellinipedretti@gmail.com>
2020-09-14 15:13:58 -0300

    testando caixa de dialogo

---
+++ script.js
@@ -0,0 +1,6 @@
+let input = document.querySelector("#text-input");
+let memeText = document.querySelector("#meme-text");
+
+input.addEventListener("keyup", function() {
+    memeText.innerText = input.value;
+});
