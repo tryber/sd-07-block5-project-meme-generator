1b85bb2df9803a817507bd74916fb87081da9374
lukeCruz <enio.cruz@yahoo.com.br>
2020-09-20 19:50:58 -0300

    testando alterações

--- script.js
+++ script.js
@@ -1,37 +1,16 @@
+const textInput = document.getElementById('text-input');
+const memeText = document.getElementById('meme-text');
+textInput.addEventListener('input', function () {
+    memeText.innerText = textInput.value;
+  });
+  
+  const imageInsert = document.querySelector('meme-insert');
 
-//inserção de texto na imagem
-const textInput = document.querySelector('#text-input');
-const memeText = document.querySelector('#meme-text');
-textInput.addEventListener('input', function() {
-memeText.innerText = textInput.value;
-});
-//Inserção de imagem no container
-const memeInsert = document.querySelector('#meme-insert');
-const memeImage = document.querySelector('#meme-image');
-memeInsert.addEventListener('change', function(event) {
 
-    memeImage.src = URL.createObjectURL(event.target.files[0]);
-    memeImage.onload = function (){
-URL.revokeObjectURL(memeImage.src);
-    };
-});
+  imageInsert.addEventListener('input', function(){
 
-const pictureOne = document.querySelector('#meme-1');
-pictureOne.addEventListener('click', function(){
-memeImage.src = pictureOne.src;
-});
+imageInsert.id = "meme-image"
+img.src = URL.createObjectURL(this.file[0]);
 
-const pictureTwo = document.querySelector('#meme-2');
-pictureOne.addEventListener('click', function(){
-memeImage.src = pictureOne.src;
-});
 
-const pictureTree = document.querySelector('#meme-3');
-pictureOne.addEventListener('click', function(){
-memeImage.src = pictureOne.src;
-});
-
-const pictureFour = document.querySelector('#meme-4');
-pictureOne.addEventListener('click', function(){
-memeImage.src = pictureOne.src;
-});
+  })
